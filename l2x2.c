13f470aac1e1647aed6c4eacdbf0b28773961652
Felipe Nunes <164574699+Coto-nete@users.noreply.github.com>
2024-09-19 21:50:34 -0300

    Update l2x2.c

--- l2x2.c
+++ l2x2.c
@@ -1,136 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Venda{
-    int nota;
-    int vendedor;
-    int data;
-    int valor;
-}temp;
+struct Nota{
+    int Num;
+    int Data;
+    int Resp;
+    float Valor;
+}nota;
 
+typedef no no;
 
-typedef struct TreeReg{
-    struct TreeReg *Pai;
-    struct TreeReg *Esq;
-    struct TreeReg *Dir;
-    struct Venda Dado;
+struct no{
+    struct Nota Dado;
+    struct no* Pai;
+    struct no* Dir;
+    struct no* Esq;
+};
+
+no *raiz;
+
+struct{
+    int Dias;
+    int Qntfunc;
 }reg;
 
+int contdias(int mes);
+void iniciar();
 
-int qntdias(int mes);
-reg* criaAB(struct Venda dado);
-int filhoEsq(reg *raiz, struct Venda dado);
-int filhoDir(reg *raiz, struct Venda dado);
-
-
+int temp;
+float ftemp;
 void main(){
-    int dias,qntfunc,temp;
+
+    {   /*Leitura do reg*/
     do{
         printf("Entre com o mes em que as vendas foram realizadas: ");
-        scanf("%i",&dias);printf("\n\n");
+        scanf("%i",&temp);printf("\n\n");
 
-        if (dias<=0 || dias>12){
+        if(temp>12 || temp<1){
             printf("Mes deve ser maior ou igual a 1 e menor ou igual a 12\n\n");
         }
-    }while(dias<=0 || dias>12);
+    }while(temp>12 || temp<1);
+    reg.Dias = contdias(temp); /*guarda os dias*/
 
-    dias = qntdias(dias);
     do{
         printf("Entre com o numero de vendedores que trabalharam no mes: ");
-        scanf("%i",&qntfunc);printf("\n\n");
+        scanf("%i",&temp);printf("\n\n");
 
-        if(qntfunc<1){
+        if(temp<=0){
             printf("Numero de vendedores deve ser maior do que zero\n\n");
         }
-    }while(qntfunc<1);
+    }while(temp<=0);
+    reg.Qntfunc = temp; /*guarda os funcionarios*/
 
+    }
+
+    {/*leitura da venda*/
+    int flag,cont;
     printf("Entre com as vendas. Para cada venda realizada no mes informar o numero da nota fiscal que a identifica, o codigo do vendedor responsavel, o dia do mes em que foi feita e o valor da venda em reais. Quando desejar finalizar a entrada de dados digite o numero zero para a nota fiscal.\n\n");
-        do{
-            printf("Venda: ");
-            scanf("%i",&temp);
-            if (temp==0){
-                continue;
-            }
+    no* raiz;
+    do{
+        flag = 0;
+        printf("Venda: ");
+        scanf("%i",&temp);
+        if (temp==0){
+            break;
+        }
+        nota.Num = temp;
 
+        scanf("%i",&temp);
+        if (temp>reg.Qntfunc){
+            printf("Codigo do vendedor invalido\n\n");
+            flag = 1; 
+        }
+        nota.Resp = temp;
 
-        }while(temp!=0);
+        scanf("%i",&temp);
+        if (temp>reg.Dias || temp<1){
+            printf("Dia da venda invalido\n\n");
+            flag = 1;
+        }
+        nota.Data = temp;
 
+        scanf("%f",&ftemp);
+        if (ftemp<=0){
+            printf("Valor da venda invalida\n\n");
+            flag = 1;
+        }
+        nota.Valor = ftemp;
+        if (flag==0){
+            inserir();
+        }
+    }while(1);
+
+    }
 }
 
-int qntdias(int mes){
-    int dias;
-    if (mes==2){
-        dias = 28;
-        return dias;
-    }
-    if (mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12){
-        dias = 31;
-        return dias;
+
+int contdias(int mes){
+    if(mes == 2){
+        return 28;
+    }else if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12){
+        return 31;
     }else{
-        dias = 30;
-        return dias;
+        return 30;
     }
-    return dias;
 }
 
-reg* criaAB(struct Venda dado){
-	reg *tree = (temp*)malloc(sizeof(temp));    // Aloca memória para o novo nó.
 
-	tree->Dado = dado;   // Define o dado do nó.
-
-	tree->Esq = NULL;    // Inicializa os filhos da esquerda e direita e o pai como NULL.
-	tree->Dir = NULL;
-	tree->Pai = NULL;
-	return tree;
+void iniciar(){
+    raiz = NULL;
 }
 
-int insereAB(reg *raiz, struct Venda dado)
-{
-	if (raiz->Dado.nota == dado.nota)
-		return 0;
-	else {
-		if (dado.nota > raiz->Dado.nota){
-			if (direita(raiz) != NULL){
-				raiz = direita(raiz);
-				insereAB(raiz, dado);
-			}
-			else {
-				filhoDir(raiz, dado);
-				return 1;
-			}
-		} else {
-			if (esquerda(raiz) != NULL){
-				raiz = esquerda(raiz);
-				insereAB(raiz, dado);
-			}
-			else {
-				filhoEsq(raiz, dado);
-				return 1;
-			}
-		}
-	}
+void inserir(no *traiz,struct Nota dado){
+	
+    if(traiz==NULL){
+        traiz = malloc(sizeof(no));
+        traiz->Esq = NULL;
+        traiz->Dir = NULL;
+        traiz->Pai = NULL;
+    }
 }
 
-int filhoEsq(reg *raiz, struct Venda dado)
-{
-	reg *node = criaAB(dado);
-	if (raiz->Esq == NULL){
-		raiz->Esq = node;
-		raiz->Esq->Pai = raiz;
-		return 1;
-	} else {
-		return 0;
-	}
-}
 
-int filhoDir(reg *raiz, struct Venda dado)
-{
-	reg *node = criaAB(dado);
-	if (raiz->Dir == NULL){
-		raiz->Dir = node;
-		raiz->Dir->Pai = raiz;
-		return 1;
-	} else {
-		return 0;
-	}
-}
